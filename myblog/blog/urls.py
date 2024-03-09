70233daa9abffd00d812bbb420004400239b5cea
1m4g1c <mirkasimovilnaz@gmail.com>
2024-03-10 00:03:40 +0300

    finally update today

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("aboutme", views.aboutme, name="aboutme")
+    path("aboutme", views.aboutme, name="aboutme"),
+    path("new_comment", views.new_comment, name="new_comment")
 ]
 
--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Post
+from .models import Post, Comment
 # Create your views here.
 def home(request):
     posts = Post.objects.all()
+    comments = Comment.objects.all()
     context = {
         'site_name': 'blogs',
-        'posts': posts
+        'posts': posts,
+        'comments': comments,
     }
     return render(request, 'index.html', context)
 
@@ -15,3 +17,10 @@ def aboutme(request):
         'site_name': 'about'
     }
     return render(request, 'aboutme.html', context)
+
+def new_comment(request):
+    text = request.GET['text']
+    post_id = request.GET['post_id']
+    comment = Comment(post_id=post_id, text=text)
+    comment.save()
+    return redirect('/')
